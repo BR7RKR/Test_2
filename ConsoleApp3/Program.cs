b9245d95cf5666d5c9815829987c35c5ef114371
BR7RKR <leomyts@gmail.com>
2021-09-16 20:04:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int c = 10;
+
+            if (c == 10)
+            {
+                Console.WriteLine(c*c);
+            }
+        }
+    }
+}
